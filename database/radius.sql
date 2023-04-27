1ae582deaa24498e0babee698c3a87aec40cb55b
meDigital Development <medigital.net@gmail.com>
2023-04-27 13:11:37 +0700

    Update database scema

--- database/radius.sql
+++ database/radius.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 25, 2021 at 08:17 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Host: localhost:3306
+-- Generation Time: Apr 27, 2023 at 06:10 AM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,149 +28,31 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', '$2y$10$yTW3kXXUK/lBD2TQb8j8Ceh3sQC3fzqniwTwoDIwfJzt8okhXDQi6');
+(1, 'admin', '$2y$10$KVNOSkgZX6g2G/vjfRkfwumJHuS7XjmuZhu3soLWGqFR715JmgSPq');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nas`
+-- Table structure for table `organization`
 --
 
-CREATE TABLE `nas` (
-  `id` int(10) NOT NULL,
-  `nasname` varchar(128) NOT NULL,
-  `shortname` varchar(32) DEFAULT NULL,
-  `type` varchar(30) DEFAULT 'other',
-  `ports` int(5) DEFAULT NULL,
-  `secret` varchar(60) NOT NULL DEFAULT 'secret',
-  `server` varchar(64) DEFAULT NULL,
-  `community` varchar(50) DEFAULT NULL,
-  `description` varchar(200) DEFAULT 'RADIUS Client'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radacct`
---
-
-CREATE TABLE `radacct` (
-  `radacctid` bigint(21) NOT NULL,
-  `acctsessionid` varchar(64) NOT NULL DEFAULT '',
-  `acctuniqueid` varchar(32) NOT NULL DEFAULT '',
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `realm` varchar(64) DEFAULT '',
-  `nasipaddress` varchar(15) NOT NULL DEFAULT '',
-  `nasportid` varchar(15) DEFAULT NULL,
-  `nasporttype` varchar(32) DEFAULT NULL,
-  `acctstarttime` datetime DEFAULT NULL,
-  `acctupdatetime` datetime DEFAULT NULL,
-  `acctstoptime` datetime DEFAULT NULL,
-  `acctinterval` int(12) DEFAULT NULL,
-  `acctsessiontime` int(12) UNSIGNED DEFAULT NULL,
-  `acctauthentic` varchar(32) DEFAULT NULL,
-  `connectinfo_start` varchar(50) DEFAULT NULL,
-  `connectinfo_stop` varchar(50) DEFAULT NULL,
-  `acctinputoctets` bigint(20) DEFAULT NULL,
-  `acctoutputoctets` bigint(20) DEFAULT NULL,
-  `calledstationid` varchar(50) NOT NULL DEFAULT '',
-  `callingstationid` varchar(50) NOT NULL DEFAULT '',
-  `acctterminatecause` varchar(32) NOT NULL DEFAULT '',
-  `servicetype` varchar(32) DEFAULT NULL,
-  `framedprotocol` varchar(32) DEFAULT NULL,
-  `framedipaddress` varchar(15) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radcheck`
---
-
-CREATE TABLE `radcheck` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `attribute` varchar(64) NOT NULL DEFAULT '',
-  `op` char(2) NOT NULL DEFAULT '==',
-  `value` varchar(253) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radgroupcheck`
---
-
-CREATE TABLE `radgroupcheck` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `groupname` varchar(64) NOT NULL DEFAULT '',
-  `attribute` varchar(64) NOT NULL DEFAULT '',
-  `op` char(2) NOT NULL DEFAULT '==',
-  `value` varchar(253) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radgroupreply`
---
-
-CREATE TABLE `radgroupreply` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `groupname` varchar(64) NOT NULL DEFAULT '',
-  `attribute` varchar(64) NOT NULL DEFAULT '',
-  `op` char(2) NOT NULL DEFAULT '=',
-  `value` varchar(253) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radpostauth`
---
-
-CREATE TABLE `radpostauth` (
-  `id` int(11) NOT NULL,
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `pass` varchar(64) NOT NULL DEFAULT '',
-  `reply` varchar(32) NOT NULL DEFAULT '',
-  `authdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radreply`
---
-
-CREATE TABLE `radreply` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `attribute` varchar(64) NOT NULL DEFAULT '',
-  `op` char(2) NOT NULL DEFAULT '=',
-  `value` varchar(253) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `radusergroup`
---
-
-CREATE TABLE `radusergroup` (
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `groupname` varchar(64) NOT NULL DEFAULT '',
-  `priority` int(11) NOT NULL DEFAULT 1
+CREATE TABLE `organization` (
+  `id` int NOT NULL,
+  `username` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `division` varchar(128) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `class` varchar(64) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `registered_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -184,112 +66,21 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `nas`
+-- Indexes for table `organization`
 --
-ALTER TABLE `nas`
+ALTER TABLE `organization`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `nasname` (`nasname`);
-
---
--- Indexes for table `radacct`
---
-ALTER TABLE `radacct`
-  ADD PRIMARY KEY (`radacctid`),
-  ADD UNIQUE KEY `acctuniqueid` (`acctuniqueid`),
-  ADD KEY `username` (`username`),
-  ADD KEY `framedipaddress` (`framedipaddress`),
-  ADD KEY `acctsessionid` (`acctsessionid`),
-  ADD KEY `acctsessiontime` (`acctsessiontime`),
-  ADD KEY `acctstarttime` (`acctstarttime`),
-  ADD KEY `acctinterval` (`acctinterval`),
-  ADD KEY `acctstoptime` (`acctstoptime`),
-  ADD KEY `nasipaddress` (`nasipaddress`);
-
---
--- Indexes for table `radcheck`
---
-ALTER TABLE `radcheck`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `username` (`username`(32));
-
---
--- Indexes for table `radgroupcheck`
---
-ALTER TABLE `radgroupcheck`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `groupname` (`groupname`(32));
-
---
--- Indexes for table `radgroupreply`
---
-ALTER TABLE `radgroupreply`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `groupname` (`groupname`(32));
-
---
--- Indexes for table `radpostauth`
---
-ALTER TABLE `radpostauth`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `radreply`
---
-ALTER TABLE `radreply`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `username` (`username`(32));
-
---
--- Indexes for table `radusergroup`
---
-ALTER TABLE `radusergroup`
-  ADD KEY `username` (`username`(32));
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `nas`
+-- AUTO_INCREMENT for table `organization`
 --
-ALTER TABLE `nas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radacct`
---
-ALTER TABLE `radacct`
-  MODIFY `radacctid` bigint(21) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radcheck`
---
-ALTER TABLE `radcheck`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radgroupcheck`
---
-ALTER TABLE `radgroupcheck`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radgroupreply`
---
-ALTER TABLE `radgroupreply`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radpostauth`
---
-ALTER TABLE `radpostauth`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `radreply`
---
-ALTER TABLE `radreply`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `organization`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
