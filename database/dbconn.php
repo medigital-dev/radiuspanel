865654d40ceb0eda2a908ae797e7c90629277304
meDigital Development <medigital.net@gmail.com>
2023-05-10 14:01:06 +0700

    Update dbconn.php
    Add default timezone

--- database/dbconn.php
+++ database/dbconn.php
@@ -1,8 +1,9 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
 
-    $server   = "localhost";
-    $user     = "root";
-    $pass     = "radpass";
-    $database = "radius";
+$server   = "localhost";
+$user     = "root";
+$pass     = "radpass";
+$database = "radius";
 
-    $conn = mysqli_connect($server, $user, $pass, $database);
+$conn = mysqli_connect($server, $user, $pass, $database);
